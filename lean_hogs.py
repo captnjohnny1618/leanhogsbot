e8dacada5064e4bd9ac4c8eae07b1946885ed279
John Hoffman <captnjohnny1618@gmail.com>
2016-06-26 17:39:39 -0700

    First commit

---
+++ lean_hogs.py
@@ -0,0 +1,22 @@
+# data_site_1='https://www.quandl.com/api/v3/datasets/ODA/PPORK_USD'
+import sys
+import os
+
+import quandl
+import twython
+
+def main():
+    quandl.get('CHRIS/CME_LN1')
+
+    
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
+
