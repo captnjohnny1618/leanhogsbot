7c5f2c31560a63d6d540c0a320ea7a1e6a8f0de6
John Hoffman <captnjohnny1618@gmail.com>
2016-06-28 23:15:09 -0700

    Basic price tweeting now set up

--- lean_hogs.py
+++ lean_hogs.py
@@ -1,12 +1,39 @@
-# data_site_1='https://www.quandl.com/api/v3/datasets/ODA/PPORK_USD'
+#!/usr/bin/env python
+
 import sys
 import os
+import time
+from datetime import date,timedelta
+
+import random
 
 import quandl
 from twython import Twython
 
 def main():
-    #quandl.get('CHRIS/CME_LN1')
+
+    # Set up our dates:
+    today=date.today()
+    a_week_ago=today-timedelta(days=7)
+    today_datestr=today.strftime("%Y-%m-%d")
+    a_week_ago_datestr=a_week_ago.strftime("%Y-%m-%d")
+
+    if today.weekday()==0 or today.weekday()==6:
+        is_weekend=True
+    else:
+        is_weekend=True
+    
+    # Grab leanhogs dataset from Quandl
+    data=[];
+    try:
+        data=quandl.get('CHRIS/CME_LN1',start_date=a_week_ago_datestr,end_date=today_datestr)
+    except: # Can't access internet; wait 30 minutes and try again
+        print("Couldn't reach Quandl, sleeping and trying again")
+        data=[];
+        time.sleep(30*60);
+
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    
     with open('.app_key','r') as f:
         APP_KEY=f.read()
     with open('.app_secret','r') as f:
@@ -30,6 +57,36 @@ def main():
         print("User secret: " + APP_SECRET)
 
     twitter=Twython(APP_KEY,APP_SECRET,OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
+
+    # Form our tweet (very basic, will be more complex in the future)
+    if not is_weekend:
+        choice=random.randint(1,1);
+    else:
+        choice=-1
+
+    if choice==-1:
+        tweet_string=form_tweet_fun_fact()
+    elif choice==1:
+        tweet_string=form_tweet_closing_price(data)
+    elif choice==2:
+        print('Ummm wat');
+        sys.exit()
+    else:
+        sys.exit("Something went wrong")
+        
+    # Send the tweet
+    send_dummy_tweet(tweet_string)
+    #send_tweet(tweet_string,twitter)
+
+def form_tweet_closing_price(data):
+    arr=data.Settle.values
+    yest_settle=arr[len(arr)-1];
+    today_settle=arr[len(arr)-2];
+    tweet_string="The lean hogs market (CME) closed at $%.2f, a %.2f%% change over the last day of trading." % (today_settle,(yest_settle-today_settle)/yest_settle*100)
+    return tweet_string
+
+def form_tweet_fun_fact():
+    return "Did you know hog is another word for pig?"
 
 def send_dummy_tweet(s):
     print(s)
